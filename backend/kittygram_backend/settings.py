28cf872c9f923819b0c79f506f91e80263c22945
NikTihomirovv <NikTihomirovv@yandex.ru>
2024-09-19 14:38:48 +0000

    try_2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,11 +1,15 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+#SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['89.169.160.21', '127.0.0.1', 'localhost','kittygramk.ydns.eu']
 
