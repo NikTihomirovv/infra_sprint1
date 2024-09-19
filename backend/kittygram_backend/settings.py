901aed79e6d97c5b7bdb898d26b7f0fb7c75b3ae
NikTihomirovv <NikTihomirovv@yandex.ru>
2024-09-19 15:11:24 +0000

    try_3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,8 +7,6 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
-
 DEBUG = False
 
 ALLOWED_HOSTS = ['89.169.160.21', '127.0.0.1', 'localhost','kittygramk.ydns.eu']
